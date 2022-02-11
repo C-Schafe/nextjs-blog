8e868544a6441f993f2b2145a9a3da9614a07fdd
jiangmingyang <louisjiangmy@gmail.com>
2022-02-11 12:40:16 +0800

    use entity and seed mock data

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-
-createConnection().then(async connection => {
-    console.log(connection);
-}).catch(error => console.log(error));
