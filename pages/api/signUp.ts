db95db91af2cd69fc39c4b50cb5430da4b92d137
jiangmingyang <louisjiangmy@gmail.com>
2022-02-21 16:07:49 +0800

    用户注册校验逻辑放到entity User内; 添加beforeInser生成密码摘要

--- pages/api/signUp.ts
+++ pages/api/signUp.ts
@@ -4,46 +4,21 @@ import { getDatabaseConnection } from '../../lib/getDatabaseConnection';
 import { User } from '../../src/entity/User';
 import md5 from 'md5';
 
-type ValidateErrors = {
-  username: string[],
-  password: string[],
-  confirmedPassword: string[],
-}
-
 const signUp:NextApiHandler = async(request, response) => {
   console.log(request.body);
-  // 校验注册信息是否合法，在数据库是否重复
-  const { username, password, confirmedPassword} = request.body;
-  const errors:ValidateErrors = {
-    username: [],
-    password: [],
-    confirmedPassword: []
-  }
-  console.log(username, password, confirmedPassword);
-  if(username.trim().length < 6) {
-    errors.username.push('用户名过短，必须大于等于6个字符');
-  }
-  if(password.length < 6) {
-    errors.password.push('密码过短，必须大于等于6个字符');
-  }
-  if(password !== confirmedPassword) {
-    errors.confirmedPassword.push('密码不匹配');
-  }
-  // 检查数据库是否有重复用户
   const connection = await getDatabaseConnection();
-  const user = await connection.manager.findOne(User, {where: {username}});
-  if(user) {
-    errors.username.push('用户已存在')
-  }
+  const { username, password, confirmedPassword} = request.body;
+  const newUser = new User();
+  newUser.username = username;
+  newUser.password = password;
+  newUser.confirmedPassword = confirmedPassword;
+  await newUser.validate();
   response.setHeader('Content-Type', 'application/json');
-  if(Object.values(errors).find(error => error.length > 0)) {
+  if(newUser.hasErrors()) {
     response.statusCode = 422;
-    response.write(JSON.stringify(errors));
+    response.write(JSON.stringify(newUser.getErrors()));
   } else {
     // 通过检查，存储用户信息
-    const newUser = new User();
-    newUser.username = username;
-    newUser.passwordDigest = md5(password);
     connection.manager.save(newUser);
     response.statusCode = 200;
   }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,14 @@
-import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {BeforeInsert, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import { getDatabaseConnection } from "../../lib/getDatabaseConnection";
 import { Comment } from "./Comment";
 import { Post } from "./Post";
+import md5 from 'md5';
+
+type ValidateErrors = {
+  username: string[],
+  password: string[],
+  confirmedPassword: string[],
+}
 
 @Entity('users')
 export class User {
@@ -18,4 +26,38 @@ export class User {
   posts: Post[]
   @OneToMany(() => Comment, comment => comment.user)
   comments: Comment[]
+  password: string
+  confirmedPassword: string
+  errors:ValidateErrors = {
+    username: [],
+    password: [],
+    confirmedPassword: []
+  }
+  async validate() {
+    if(this.username.trim().length < 6) {
+      this.errors.username.push('用户名过短，必须大于等于6个字符');
+    }
+    if(this.password.length < 6) {
+      this.errors.password.push('密码过短，必须大于等于6个字符');
+    }
+    if(this.password !== this.confirmedPassword) {
+      this.errors.confirmedPassword.push('密码不匹配');
+    }
+    // 检查数据库是否有重复用户
+    const connection = await getDatabaseConnection();
+    const user = await connection.manager.findOne(User, {where: {username: this.username}});
+    if(user) {
+      this.errors.username.push('用户已存在')
+    }
+  }
+  hasErrors() {
+    return Object.values(this.errors).find(error => error.length > 0);
+  }
+  getErrors() {
+    return this.errors;
+  }
+  @BeforeInsert()
+  generatePasswordDigest() {
+    this.passwordDigest = md5(this.password);
+  }
 }
