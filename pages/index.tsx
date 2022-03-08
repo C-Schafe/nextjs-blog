24535ff5ebe618b39f15a9b93f93ea86c44ec88c
jiangmingyang <louisjiangmy@gmail.com>
2022-03-08 16:34:08 +0800

    首页logo样式调整

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 // asset
 import imageAsset from '../assets/image/sheep_baw.png';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <img src={imageAsset.src} height={300}/>
+        <Image src={imageAsset.src} height={200} width={190}></Image>
         <h1>JMY 的个人博客</h1>
         <p>我在青青草原吃草</p>
         <Link href="/posts">
