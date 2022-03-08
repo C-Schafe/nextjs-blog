379e8b3b674f0a10955daaaf9a56df248cc14ef8
jiangmingyang <louisjiangmy@gmail.com>
2022-03-08 18:02:50 +0800

    更新readme；新增部署脚本

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,10 @@
-echo hi > /tmp/jiang
+docker restart psql1 &&
+cd /home/blog/app &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker kill nextjs-blog &&
+docker rm nextjs-blog &&
+docker build . -t jiang/node-web-app &&
+docker run --name=nextjs-blog --network=host -p 3000:3000 -d jiang/node-web-app &&
+echo "deploy successfully!"
