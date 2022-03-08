51a2d1c0976bb37cadd1d77fd0a80d4912de39aa
jiangmingyang <louisjiangmy@gmail.com>
2022-03-08 18:11:07 +0800

    修改首页文本

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         <Image src={imageAsset.src} alt='homepage-logo' height={200} width={190}></Image>
         <h1>JMY 的个人博客</h1>
         <p>我在青青草原吃草</p>
+        <p>我在青青草原奔跑</p>
         <Link href="/posts">
           <a><u>博客列表</u></a>
         </Link>
