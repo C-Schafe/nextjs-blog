db36ec39ffc75ddc126bbac94183782528d06a03
jiangmingyang <louisjiangmy@gmail.com>
2022-03-08 18:48:28 +0800

    更新deploy脚本，添加migration相关内容

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,10 @@ cd /home/blog/app &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
+git apply migration.patch;
+yarn migration:build &&
+yarn m:run &&
+git reset --hard HEAD &&
 docker kill nextjs-blog;
 docker rm nextjs-blog;
 docker build . -t jiang/node-web-app &&
