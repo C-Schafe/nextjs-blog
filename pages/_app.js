16c59264f553b46248ef6cbc54953cd8d1789d78
jiangmingyang <louisjiangmy@gmail.com>
2022-03-09 18:19:15 +0800

    删除errorBoundary

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@
 import Head from 'next/head';
 import '../styles/globals.css';
 import 'github-markdown-css';
-import { ErrorBoundary } from '../pages/error/errorBoundary';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,9 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>我的博客 - JMY</title>
       </Head>
-      <ErrorBoundary>
         <Component {...pageProps} />
-      </ErrorBoundary>
     </>
   )
 }
