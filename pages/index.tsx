41382008bafe5b6eeac2fbb299fe5cd40bacf644
jiangmingyang <louisjiangmy@gmail.com>
2022-03-12 23:19:11 +0800

    修改首页文字

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <h1>JMY 的个人博客</h1>
         <p>我在青青草原吃草</p>
         <p>我在青青草原奔跑</p>
+        <p>我在青青草原晒太阳</p>
         <Link href="/posts">
           <a><u>博客列表</u></a>
         </Link>
